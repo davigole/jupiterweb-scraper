ddb10d2e61842316ed7b94c53cabd2eb923fb04f
Davi Golebiovski <davigolebiovski@gmail.com>
2026-05-26 14:06:58 -0300

    Correção de erro em URL

--- jupiter/urls.py
+++ jupiter/urls.py
@@ -3,7 +3,7 @@ from urllib.parse import urljoin
 URL_BASE = "https://uspdigital.usp.br/jupiterweb/"
 URLS: dict[str, str] = {
     "listagem": urljoin(URL_BASE, "jupDisciplinaLista?codcg={codigo}&letra=0-Z&tipo=D"),
-    "disciplina": urljoin(URL_BASE, "obterDisciplina?sgldis={sigla}&verdis=1"),
+    "disciplina": urljoin(URL_BASE, "obterDisciplina?sgldis={sigla}"),
     "oferecimento": urljoin(URL_BASE, "obterTurma?sgldis={sigla}"),
     "requisitos": urljoin(URL_BASE, "listarCursosRequisitos?coddis={sigla}"),
     "institutos": urljoin(URL_BASE, "jupColegiadoLista?tipo=D"),
